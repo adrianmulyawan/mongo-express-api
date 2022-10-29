b808f86cf5d90182e5198e18a5398d4a5ed8959b
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-10-29 12:56:41 +0700

    edit config express

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ const port = 3000;
 // => Parsing Request -> JSON
 app.use(express.json());
 // => Parsing Request Form URL Encoded: Upload File, Kirim Data Dari Form
-app.use(express.urlencoded());
+app.use(express.urlencoded({
+	extended: true
+}));
+
+// # Create End Point / Routing
+app.get('/', (req, res) => {
+	res.status(200);
+	res.json({
+		message: "Welcome to Express API Tutorial!"
+	});
+});
 
 app.listen(port, () => {
-    console.info(`Mongo Express API Listening in http://localhost:${port}/`);
+  console.info(`Mongo Express API Listening in http://localhost:${port}/`);
 });
